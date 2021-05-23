09207bb41bdad5e0836a208a71d787d84e11fd37
rrry8 <2016009@s.asojuku.ac.jp>
2021-05-23 22:02:50 +0900

    skelton追加

---
+++ SkeltonProject/main.cpp
@@ -0,0 +1,19 @@
+#include<DxLib.h>
+
+int WINAPI WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine,
+    int nCmdShow)
+{
+    ChangeWindowMode(true);
+    DxLib_Init();
+    SetDrawScreen(DX_SCREEN_BACK);
+    while (ProcessMessage() != -1)
+    {
+        ClearDrawScreen();
+        ScreenFlip();
+    }
+    DxLib_End();
+    return 0;
+}
